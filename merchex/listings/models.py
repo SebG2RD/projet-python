5e81af16f6c3a4b518c8e107fd0e624e66426274
Seb-Gerard <sebastien.gerard@cnpp.com>
2025-02-25 17:10:04 +0100

    Modification de la base de donnée

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Band(models.Model):
-    name = models.fields.CharField(max_length=100)
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNT_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+    name = models.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900),MaxValueValidator(2024)])
+    active = models.fields.BooleanField(default=True)
+    official_website = models.fields.URLField(null=True, blank=True)
     
 class Listing(models.Model):
     name = models.fields.CharField(max_length=100)

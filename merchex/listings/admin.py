a6d127f5f8f98d247f03b241f300ced646c1d63c
Seb-Gerard <sebastien.gerard@cnpp.com>
2025-02-26 11:30:23 +0100

    modification de la base de donnée

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,13 @@
+from re import L
 from django.contrib import admin
 
-# Register your models here.
+from .models import Band, Listing
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'genre', 'year_formed', 'active', 'official_website')
+    
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'sold', 'year', 'type','band')
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Listing, ListingAdmin)
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -13,6 +13,21 @@ class Band(models.Model):
     active = models.fields.BooleanField(default=True)
     official_website = models.fields.URLField(null=True, blank=True)
     
+    def __str__(self):
+        return self.name
+    
 class Listing(models.Model):
+    class ListingType(models.TextChoices):
+        ALBUM = 'AL'
+        SINGLE = 'SI'
+        EP = 'EP'
+        COMPILATION = 'CO'
     name = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(validators=[MinValueValidator(1900),MaxValueValidator(2024)])
+    type = models.fields.CharField(choices=ListingType.choices, max_length=5)
+    def __str__(self):
+        return self.name
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
     
